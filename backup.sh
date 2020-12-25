6dd65676c5016fa73ebc64e8c22d176fe8124909
Ingo Eichhorst <ingo.eichhorst@nowtilus.tv>
2020-12-25 22:21:54 +0100

    updated backup strategy

--- backup.sh
+++ backup.sh
@@ -5,4 +5,8 @@
 # In /etc/crontab
 # 0 4 * * *     /home/pi/modbus-reader/backup.sh
 
-influxd backup -portable -database energy -start $(date --date="${dataset_date} -1 day" --utc +%FT%TZ) /mnt/backup
+# Old backup strategty causing to many disc space used
+# influxd backup -portable -database energy -start $(date --date="${dataset_date} -1 day" --utc +%FT%TZ) /mnt/backup
+
+# New backup strategy whick overwrites every day
+influxd backup -portable -database energy /mnt/backup
